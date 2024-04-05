6baedad8a0fe67e89fc546b2b55a9009fdf5ba2a
Vaibhav6644 <162414390+Vaibhav6644@users.noreply.github.com>
2024-04-05 12:08:39 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,9 +24,13 @@ export const Navbar = () => {
           {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, index)=>
             (<NavbarItem key={item+index} title={item}/>)
           )}
-          <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
-            Login
-          </li>
+          { !currentAccount &&(
+            <button type='button' onClick={connectWallet}
+              className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
+            >
+              <p className="text-white text-base font-semibold ">Connect Wallet</p>
+            </button>
+          )}
         </ul>
         <div className="flex relative">
           {toggleMenu
